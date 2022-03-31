2c193824f583a588af662ac838b6c5cabf4133ca
Andrii <andriyparayko2017@gmail.com>
2022-03-31 23:24:10 +0300

    Test

--- lab_book_task/Test.py
+++ lab_book_task/Test.py
@@ -2,16 +2,6 @@
 print("¯\_(ツ)_/¯")
 
 
-x = float(input())  # 45
-y = float(input())  # 32
-
-if x >= 1 and x <= 2.5 and y <= -2:
-    print(True)
-elif y >= 1.5:
-    print(True)
-else:
-    print(False)
-
 
 
 
